2885d73f9417da15513802407546206e7618ec98
viswam <vbogaraj@cisco.com>
2020-07-03 08:08:07 +0530

    Updates

---
+++ precache-manifest.6f90a4b4399606e1490be90a82c23d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558d7e76175d12b7353d43f385bb9ef7",
+    "url": "/react-projects/index.html"
+  },
+  {
+    "revision": "b4fff079a499929fe409",
+    "url": "/react-projects/static/css/main.12618d2d.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/react-projects/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-projects/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fff079a499929fe409",
+    "url": "/react-projects/static/js/main.2ad1501d.chunk.js"
+  },
+  {
+    "revision": "f1ea1f983862d15db00e",
+    "url": "/react-projects/static/js/runtime-main.f3acac88.js"
+  }
+]);
